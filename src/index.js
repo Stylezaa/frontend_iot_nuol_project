2487a653a146a80826e3fced367c1a818118e52c
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-05-22 15:53:58 +0700

    Remove Tash Page

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import '../src/assets/font/NotoSansLao-Medium.ttf'
 import PrivateRoute from './Guard/auth';
 import './index.css';
 import MapComponent from './Components/Map/MapComponent'
-// import Home from './Pages/Home'
-import Map from './Pages/Map'
-import Sensor from './Pages/Sensor'
-import About from './Pages/About'
 import Overview from './Pages/Admin/Overviews'
 import SensorDash from './Pages/Admin/Sensor'
 import MapDash from './Pages/Admin/Map'
@@ -20,17 +16,7 @@ root.render(
   <Router>
     <Switch>
       <Route path="/" exact>
-        {/* <Home /> */}
         <MapComponent />
-      </Route>
-      <Route path="/map" exact>
-        <Map />
-      </Route>
-      <Route path="/sensor" exact>
-        <Sensor />
-      </Route>
-      <Route path="/about" exact>
-        <About />
       </Route>
       <PrivateRoute path="/dashboard/overview" exact>
         <Overview />

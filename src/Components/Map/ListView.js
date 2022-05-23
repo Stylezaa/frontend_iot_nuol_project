115793390a29f2274c47565d3a0986a4da383846
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-05-23 15:38:24 +0700

    Update ListView.js

--- src/Components/Map/ListView.js
+++ src/Components/Map/ListView.js
@@ -100,13 +100,13 @@ function ListView(props) {
             {isAuth
                 ? 
                 <>          
-                    <div className="lg:hidden shadow-lg absolute cursor-pointer rounded-r -right-16 p-0 m-0 top-20 w-16 h-12 font-mediu bg-white">
+                    <div className="lg:hidden shadow-lg absolute cursor-pointer rounded-r -right-16 p-0 m-0 top-20 w-16 h-12 font-medium bg-blue-500">
                         <ProfileControl link="/dashboard/overview" title="ໜ້າຈັດການລະບົບ" />
                     </div>        
                 </>
                 : 
                 <>
-                    <a href="/login" className="lg:hidden flex items-center justify-center text-blue-500 shadow-lg absolute cursor-pointer rounded-r -right-10 p-0 m-0 top-20 w-10 h-10 font-mediu bg-white">
+                    <a href="/login" className="lg:hidden flex items-center justify-center text-white shadow-lg absolute cursor-pointer rounded-r -right-10 p-0 m-0 top-20 w-10 h-10 font-medium bg-blue-500">
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
                             <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>

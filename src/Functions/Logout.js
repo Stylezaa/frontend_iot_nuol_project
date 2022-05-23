6eceb2ea7249b4b6079425a0d9517bd419578fe9
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-05-23 20:40:09 +0700

    Clean Code 1.1

--- src/Functions/Logout.js
+++
@@ -1,9 +0,0 @@
-import { useHistory } from 'react-router-dom';
-
-export default function Logout() {
-    const history = useHistory();
-    
-    localStorage.removeItem('token');
-    localStorage.removeItem('Profile');
-    history.replace('/');
-}

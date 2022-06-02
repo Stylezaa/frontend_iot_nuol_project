c8caf4d2b6aa9956af166a269d8de8f409d1dcdc
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-06-02 14:15:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../src/assets/font/NotoSansLao-Medium.ttf'
+import '../src/assets/font/NotoSansLao-Medium.ttf';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

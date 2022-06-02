324f4a15a10fdc6bf391827fda32acac985770a0
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-06-02 11:30:48 +0700

    Revert "Add Config Google Analytic"
    This reverts commit 7339082e8ee9521a7199ac8bd6e4002bbc59d7c1.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ReactGa from 'react-ga'
 import '../src/assets/font/NotoSansLao-Medium.ttf'
 import PrivateRoute from './Guard/auth';
 import './index.css';
@@ -12,14 +11,6 @@ import Login from './Pages/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  useEffect(() =>{
-    ReactGa.initialize('G-EL6NPG0LVM')
-
-    //to report page view
-    ReactGa.pageview(window.location.pathname + window.location.search);
-  }, [])
-
   <Router>
     <Switch>
       <Route path="/" exact>

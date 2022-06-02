7339082e8ee9521a7199ac8bd6e4002bbc59d7c1
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-06-02 11:30:20 +0700

    Add Config Google Analytic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ReactGa from 'react-ga'
 import '../src/assets/font/NotoSansLao-Medium.ttf'
 import PrivateRoute from './Guard/auth';
 import './index.css';
@@ -11,6 +12,14 @@ import Login from './Pages/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+  useEffect(() =>{
+    ReactGa.initialize('G-EL6NPG0LVM')
+
+    //to report page view
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   <Router>
     <Switch>
       <Route path="/" exact>

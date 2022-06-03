6f23ab6c5a4974c850ac30c75fd0df48bc7153d8
Two Archineer <archineertwo@Twos-Mac-mini.local>
2022-06-03 15:07:09 +0700

    Update Embed

--- src/Pages/Admin/Station.js
+++ src/Pages/Admin/Station.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Embed from 'react-embed';
 import SidebarComponent from '../../Components/Admin/SidebarComponent'
 import BarComponent from '../../Components/Admin/BarComponent'
 import CurrentTime from '../../Components/CurrentTime'
@@ -25,6 +26,8 @@ export default function DashboardContent() {
         <div className="container mx-auto text-center mt-24 lg:px-5">
           <iframe className="rounded hidden lg:block" width="100%"  height="600px" src="http://202.137.130.47:1880/#flow/03c9ea59963c8365" title="Node-Red" allowFullScreen>
 					</iframe>
+          <Embed url='http://202.137.130.47:1880/#flow/03c9ea59963c8365' />
+          <Embed url='https://twitter.com/hercuppacoffee/status/911958476678561792' />
           <span className="lg:hidden w-full shadow-md bg-rose-300 text-white px-4 py-2 rounded-sm">ໜ້ານີ້ສາມາດເບິ່ງໄດ້ສະເພາະໂໝດ Desktop ເທົ່ານັ້ນ</span>
         </div>
       </div>

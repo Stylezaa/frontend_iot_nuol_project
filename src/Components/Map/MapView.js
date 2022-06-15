8996a27753bb856e9f4620b7283b7916409e4c39
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-06-15 10:23:38 +0700

    Change Icon

--- src/Components/Map/MapView.js
+++ src/Components/Map/MapView.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet';
 import { divIcon } from 'leaflet';
+import axios from 'axios';
 
 const icons = {
 	OnlineIcon: divIcon({className: 
-        'animate-pulse duration-300 border-2 border-solid rounded-full bg-blue-300/60 border-blue-500 after:absolute after:bg-blue-500 after:rounded-full after:animate-ping after:w-full after:h-full',
+        'onlineStatus animate-pulse duration-300 border-2 border-solid rounded-full bg-blue-300/60 border-blue-500 after:absolute after:bg-blue-500 after:rounded-full after:animate-ping after:w-full after:h-full',
          iconSize: [96, 96]}),
     OfflineIcon: divIcon({className: 
-        'animate-pulse duration-300 border-2 border-solid rounded-full bg-red-300/60 border-red-500 after:absolute after:bg-red-500 after:rounded-full after:animate-ping after:w-full after:h-full',
+        'offlineStatus animate-pulse duration-300 border-2 border-solid rounded-full bg-red-300/60 border-red-500 after:absolute after:bg-red-500 after:rounded-full after:animate-ping after:w-full after:h-full',
          iconSize: [96, 96]}),
 };
 
+let markerIcon;
+
 function MapView(props) {
     const { locationOne, mapCenter, onSelectMarker } = props;
+
+    const [status, setStatus] = React.useState([]);
+    // console.log(status);
+
+    const getStatus = async () => {
+        try {
+            const resp = await axios.get('https://ceit-iot-api.herokuapp.com/api/station/1/filter/1');
+            setStatus(resp.data.station_status[0].status);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    React.useEffect(() => {
+      getStatus();
+    
+        const interval = setInterval(() => {
+          getStatus();
+        }, 10000);
+    
+        return () => clearInterval(interval);
+    }, []);
+
+    if (status === "Online") {
+        markerIcon = icons.OnlineIcon;
+    } else {
+        markerIcon = icons.OfflineIcon;
+    }
 
     const markerElements = locationOne.map(location => {
         const {
             id, name,
             coordinates: { latitude, longitude },
         } = location;
-
-        let markerIcon = icons.OnlineIcon;
 
         return (
             <Marker 

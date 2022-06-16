59b290019209ec5b56f8a8dd2fa08dda0347e4e0
DevSin <48406477+Stylezaa@users.noreply.github.com>
2022-06-16 22:32:04 +0700

    Fix Route not go by remove <React.StrictMode>
    Fix Route not go when click I must refresh page for to go another page

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '../src/assets/font/NotoSansLao-Medium.ttf'
-import PrivateRoute from './Guard/auth';
+import '../src/assets/font/NotoSansLao-Medium.ttf';
 import './index.css';
-import MapComponent from './Components/Map/MapComponent';
-import Overview from './Pages/Admin/Overviews'
-import Station from './Pages/Admin/Station'
-import Login from './Pages/Login'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Switch>
-      <Route path="/" exact>
-        <MapComponent />
-      </Route>
-      <PrivateRoute path="/dashboard/overview" exact>
-        <Overview />
-      </PrivateRoute>
-      <PrivateRoute path="/dashboard/station" exact>
-        <Station />
-      </PrivateRoute>
-      <Route path="/login" exact>
-        <Login />
-      </Route>
-    </Switch>
-  </Router>,
+    <App />
 );

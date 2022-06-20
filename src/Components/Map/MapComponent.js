f6e0ae183b7350f4c8ea56159335b9985cfdb6d2
Two Archineer <archineertwo@Twos-Mac-mini.local>
2022-06-20 16:21:28 +0700

    Fix Loading Mobile have gap

--- src/Components/Map/MapComponent.js
+++ src/Components/Map/MapComponent.js
@@ -53,7 +53,7 @@ function MapComponent() {
 	if (selectedLocation != null) {
 		detailsView = <DetailsView location={selectedLocation} onClickClose={onDeselectLocation} />
 	}
-	if (loading) {
+	if (!loading) {
 		return (
 			<>
 				<div className="hidden lg:flex space-x-4 absolute top-0 left-0 right-0 bottom-0 h-screen w-screen bg-slate-400">
@@ -88,7 +88,7 @@ function MapComponent() {
 						ກໍາລັງໂຫລດ...
 					</div>
 				</div>
-				<div className="lg:hidden container flex flex-col justify-center items-center text-white space-x-4 bg-blue-500 h-screen w-screen">
+				<div className="lg:hidden container flex absolute flex-col justify-center items-center text-white space-x-4 bg-blue-500 h-screen w-screen">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="animate-pulse h-24 w-24 mb-2 text-white" viewBox="0 0 16 16">
 						<path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6ZM6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13Z"/>
 					</svg>
